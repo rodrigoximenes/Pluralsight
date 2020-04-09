d5ca7c9277fb5aa095c61cb62253a646fffe66d4
Rodrigo <Rodrigo>
2020-04-09 11:13:25 -0300

    Mais sidenav

--- Pluralsight/src/app/app.module.ts
+++ Pluralsight/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +19,7 @@ import { SharedComponent } from './shared/shared.component';
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
