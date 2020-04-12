b8635838e72d2fd6384a361b75c28cf7d9cb8e17
Rodrigo <Rodrigo>
2020-04-12 13:06:23 -0300

    Tabela

--- Pluralsight/src/app/products/product-list/product-list.component.ts
+++ Pluralsight/src/app/products/product-list/product-list.component.ts
@@ -11,12 +11,6 @@ import { Product } from '../product';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  colunasTabela: string[] = ['productName', 'productCode', 'category', 'price'];
-  produtossss: Product[] = [];
-  teste = this.produtossss;
-
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataASource = ELEMENT_DATA;
 
   private categorySelectedSubject = new BehaviorSubject<number>(0);
   categorySelectedAction$ = this.categorySelectedSubject.asObservable();
@@ -36,32 +30,14 @@ export class ProductListComponent implements OnInit {
     })
   );
   
-  
+  colunasTabela: string[] = ['productName', 'productCode', 'category', 'price'];
+  dataASource = this.products$;
 
   constructor(
     private productService: ProductService,
     private productCategoryService: ProductCategoryService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
