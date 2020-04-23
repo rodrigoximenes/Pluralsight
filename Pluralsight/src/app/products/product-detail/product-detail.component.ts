aaac2ba761d6e675574bf016960c6e3339fe8345
Rodrigo <Rodrigo>
2020-04-23 17:19:21 -0300

    detalhes

--- Pluralsight/src/app/products/product-detail/product-detail.component.ts
+++ Pluralsight/src/app/products/product-detail/product-detail.component.ts
@@ -1,28 +1,52 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../product.service';
+import { takeWhile } from 'rxjs/operators';
 import { Product } from '../product';
-import { takeWhile, tap } from 'rxjs/operators';
-import { pipe } from 'rxjs';
+import { ProductService } from '../product.service';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent implements OnDestroy {
+export class ProductDetailComponent implements OnInit,OnDestroy {
   produtoSelecionado: Product;
   id: number;
   componenteAtivo: boolean = true;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute
   ) {
     this.route.paramMap.subscribe((params) => {
-      (this.id = +params.get('id')), console.log(this.id);
+      (this.id = +params.get('id'))
     });
+  }
 
+  ngOnInit(): void {
     this.productService
       .getProduct(this.id)
       .pipe(takeWhile(() => this.componenteAtivo))
--- Pluralsight/src/app/products/product.module.ts
+++ Pluralsight/src/app/products/product.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -54,11 +55,12 @@ import { ProductResolver } from './product-resolver.service';
     MatListModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatGridListModule,
     CommonModule,
     MatTableModule,
     MatExpansionModule,
     MatSelectModule,
-    FormsModule
+    FormsModule,
   ],
   declarations: [
     ProductListComponent,
